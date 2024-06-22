361958e079846617bbf64bde667e90d696998a4b
caruazu <gustavocaruazu@gmail.com>
2024-06-22 15:33:56 -0300

    Página de Login e cadastro

--- frontend/agendadorFrontend/src/api.js
+++ frontend/agendadorFrontend/src/api.js
@@ -2,12 +2,13 @@ import axios from "axios";
 import { ACCESS_TOKEN } from "./constants";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
+  baseURL: import.meta.env.VITE_API_URL
 });
 
 api.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem(ACCESS_TOKEN);
+    
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
